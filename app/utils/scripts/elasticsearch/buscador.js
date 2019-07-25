c3d658e0ba3276a03a9b4a4a688bab1ce0f7d1d9
HugoRoca <hugo.rock20@hotmail.com>
2019-07-25 18:12:38 -0500

    avance

--- app/utils/scripts/elasticsearch/buscador.js
+++ app/utils/scripts/elasticsearch/buscador.js
@@ -9,8 +9,26 @@ module.exports = class ElasticsearchQuerys {
 
     }
 
-    aggs() {
-        
+    aggregations() {
+        if (this.params.filtroDatosSQL.length === 0) return [];
+
+        let filtros = utils.distinctInArray(this.params.filtroDatosSQL, 'ElasticsearchCampo');
+        let aggs = '{';
+        let coma = 0;
+        let j = 0;
+
+        for (let index = 0; index < filtros.length; index++) {
+            const element = filtros[index];
+            if (coma > 0) aggs += ',';
+            if (element.ElasticsearchOperador === 'term') {
+                aggs = `"${element.}"`;
+            }
+
+            coma++;
+        }
+
+        aggs += '}';
+        return JSON.parse(aggs);
     }
 
 }
