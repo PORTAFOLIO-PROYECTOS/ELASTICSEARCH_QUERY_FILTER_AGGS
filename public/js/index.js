284bca768977b3151f9fd35cd8bafa143c7d003f
Hugo <hugo.rock20@hotmail.com>
2019-07-28 01:34:27 -0500

    petición fetch

---
+++ public/js/index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const index = (() => {
+
+    const element = {
+        templateFiltroMain: '#filtroTreeViewMain',
+        templateFiltroList: '#filtroTreeViewList',
+        templateCajaProducto: '#cajaProducto',
+        mostrarFiltro: '#mostrarFiltro',
+        mostrarCajaProducto: '#mostrarCajaProducto'
+    }
+
+    const utils = {
+        mostrarFiltros: (data => {
+            let main = Handlebars.compile($(element.templateFiltroMain).html());
+            $(element.mostrarFiltro).html(main({
+                items: data
+            }));
+        }),
+
+        mostrarCajaProducto: (data => {
+            let main = Handlebars.compile($(element.templateCajaProducto).html());
+            $(element.mostrarCajaProducto).html(main({
+                items: data
+            }));
+        })
+    };
+
+    const service = {
+        cargarDatos: () => {
+            let data = {
+                "textoBusqueda": "",
+                "filtros": []
+            };
+
+            fetch('/filtros', {
+                    method: 'POST',
+                    body: JSON.stringify(data),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(res => res.json())
+                .catch(err => console.error('Error:', err))
+                .then(response => {
+                    utils.mostrarFiltros(response);
+                    utils.mostrarCajaProducto(response);
+                });
+        }
+    }
+
+    const init = (() => {
+        service.cargarDatos();
+    });
+
+    return {
+        init: init
+    }
+})();
+
+(() => {
+    index.init();
+})();
